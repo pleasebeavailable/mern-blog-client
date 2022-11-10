b6f9cfdef0985beea736295231bfd6fb1a46ee8d
Ante Šućur <Ante.Sucur@internationalvalueservices.com>
2022-11-10 17:14:21 +0100

    Implement login component

---
+++ src/app/components/log-in-components/login-container.js
@@ -0,0 +1,135 @@
+import React from "react";
+import LoginRequest from "../../model/login-request";
+import LoginForm from "./login-form";
+import {login} from "../../services/registration-service";
+import {loginAction} from "../../redux/actions/user";
+import {connect} from "react-redux";
+
+const axios = require("axios");
+const FormValidators = require("./validate");
+const validateLoginForm = FormValidators.validateLoginForm;
+const zxcvbn = require("zxcvbn");
+
+class LoginContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: {},
+      user: new LoginRequest("", ""),
+      btnTxt: "show",
+      type: "password",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.pwHandleChange = this.pwHandleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user,
+    });
+  }
+
+  pwHandleChange(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user,
+    });
+
+    if (event.target.value === "") {
+      this.setState((state) =>
+          Object.assign({}, state, {
+            score: "null",
+          })
+      );
+    } else {
+      var pw = zxcvbn(event.target.value);
+      this.setState((state) =>
+          Object.assign({}, state, {
+            score: pw.score + 1,
+          })
+      );
+    }
+  }
+
+  submitLogin(user) {
+    login(user)
+    .then((res) => {
+      if (res.success === true) {
+        this.props.loginUser({isAuthenticated: true, user: user});
+        localStorage.token = res.token;
+        localStorage.isAuthenticated = true;
+        localStorage.user = user;
+        window.location.replace("/");
+      } else {
+        this.setState({
+          errors: {message: res.data.message},
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Sign up data submit error: ", err);
+    });
+  }
+
+  validateForm(event) {
+    event.preventDefault();
+    var payload = validateLoginForm(this.state.user);
+    if (payload.success) {
+      this.setState({
+        errors: {},
+      });
+      var user = {
+        email: this.state.user.username,
+        password: this.state.user.password,
+      };
+      this.submitLogin(user);
+    } else {
+      const errors = payload.errors;
+      this.setState({
+        errors,
+      });
+    }
+  }
+
+  pwMask(event) {
+    event.preventDefault();
+    this.setState((state) =>
+        Object.assign({}, state, {
+          type: this.state.type === "password" ? "input" : "password",
+          btnTxt: this.state.btnTxt === "show" ? "hide" : "show",
+        })
+    );
+  }
+
+  render() {
+    return (
+        <div>
+          <LoginForm
+              onSubmit={this.validateForm}
+              onChange={this.handleChange}
+              onPwChange={this.pwHandleChange}
+              errors={this.state.errors}
+              user={this.state.user}
+              type={this.state.type}
+              pwMask={this.pwMask}
+          />
+        </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  loginUser: (data) => dispatch(loginAction(data))
+});
+
+export default connect(null, mapDispatchToProps)(LoginContainer);
---
+++ src/app/components/log-in-components/login-form.js
@@ -0,0 +1,52 @@
+import React from "react";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
+import "./style.css";
+
+const LoginForm = ({
+                     onSubmit,
+                     onChange,
+                     errors,
+                     user,
+                     type,
+                     onPwChange,
+                   }) => {
+  return (
+      <div className="loginBox">
+        <h1>Log In</h1>
+        {errors.message && <p style={{color: "red"}}>{errors.message}</p>}
+
+        <form onSubmit={onSubmit}>
+          <TextField
+              name="username"
+              floatingLabelText="username/email"
+              value={user.username}
+              onChange={onChange}
+              errorText={errors.username}
+          />
+          <TextField
+              type={type}
+              name="password"
+              floatingLabelText="password"
+              value={user.password}
+              onChange={onPwChange}
+              errorText={errors.password}
+          />
+
+          <br/>
+          <RaisedButton
+              className="logInSubmit"
+              primary={true}
+              type="submit"
+              label="submit"
+          />
+        </form>
+        <p>
+          Don't have an account? <br />
+          <a href="/signUp">Create account here</a>
+        </p>
+      </div>
+  );
+};
+
+export default LoginForm;
