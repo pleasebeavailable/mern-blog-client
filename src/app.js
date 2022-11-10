fbe51607fc3fec0eafa5854fb93cc1d762802988
Ante Šućur <Ante.Sucur@internationalvalueservices.com>
2022-11-10 16:29:25 +0100

    Add routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,47 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import StartingPage from "./app/components/starting-page.js";
+// import SignupContainer from "./app/components/sign-up-components/signup-container.js";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// import LoginContainer from "./app/components/log-in-components/login-container.js";
+
+const theme = createTheme();
+
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Container m-2="true">
+          <MuiThemeProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline/>
+              <Routes>
+                <Route
+                    path="/"
+                    element={
+                      <StartingPage/>
+                    }
+                ></Route>
+                <Route
+                    path="/signUp"
+                    element={
+                      {/*<SignupContainer></SignupContainer>*/}
+                    }
+                ></Route>
+                <Route
+                    path="/login"
+                    element={
+                      {/*<LoginContainer></LoginContainer>*/}
+                    }
+                ></Route>
+              </Routes>
+            </ThemeProvider>
+          </MuiThemeProvider>
+        </Container>
+      </Router>
   );
 }
 
